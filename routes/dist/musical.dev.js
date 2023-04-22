39e44ef543a02665303390eb20a5e54de250553b
Daniel1012345 <72279708+Daniel1012345@users.noreply.github.com>
2023-04-21 23:07:27 -0500

    updated project

--- routes/dist/musical.dev.js
+++ routes/dist/musical.dev.js
@@ -8,7 +8,17 @@ var router = express.Router();
 /* GET costumes */
 
 router.get('/musical/:id', musical_controllers.musical_detail);
+router.get('/create', musical_controllers.musical_create_Page);
+/* GET detail costume page */
+
+router.get('/detail', musical_controllers.musical_view_one_Page);
 module.exports = router;
+/* GET create update page */
+
+router.get('/update', musical_controllers.musical_update_Page);
+/* GET delete costume page */
+
+router.get('/delete', musical_controllers.musical_delete_Page);
 /*
 var express = require('express');
 var router = express.Router();
--- routes/musical.js
+++ routes/musical.js
@@ -3,13 +3,16 @@ const musical_controllers= require('../controllers/musical');
 var router = express.Router();
 
 /* GET costumes */
+router.get('/', function(req, res, next) {
+  res.render('musical', { title: 'Seach Results musicals'});
+});
+
 router.get('/musical/:id', musical_controllers.musical_detail );
 
 router.get('/create', musical_controllers.musical_create_Page);
 
 /* GET detail costume page */
 router.get('/detail', musical_controllers.musical_view_one_Page);
-module.exports = router;
 
 /* GET create update page */
 router.get('/update', musical_controllers.musical_update_Page);
@@ -20,10 +23,10 @@ router.get('/delete', musical_controllers.musical_delete_Page);
 /*
 var express = require('express');
 var router = express.Router();
-
-//GET home page. 
-router.get('/', function(req, res, next) {
-  res.render('musical', { title: 'Seach Results musicals'});
-});
-
 */
+//GET home page. 
+
+module.exports = router;
+
+
+
